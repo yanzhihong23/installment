ad0fbbc6f2e6822ad80b87277b1a5381f94e7bc0
Hom <yanzhihong23@qq.com>
2015-09-10 13:27:07 +0800

    random openId and orderId

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -6,8 +6,6 @@
     .constant('HOST', 'http://openapi.test.nonobank.com')
     .constant('MERCHANT', '10000')
     .constant('PRODUCTID', '89')
-    // .value('OPENID', '9527')
-    // .value('ORDERID', '89757')
     .factory('ORDER', function($location, $log, utils, localStorageService) {
         var search = utils.getLocationSearch(),
             $search = $location.search(),
@@ -15,15 +13,15 @@
 
         if(!order) {
           order = {
-            openId: search.openId || $search.openId || '9527',
-            orderId: search.orderId || $search.orderId || '89757'
+            openId: search.openId || $search.openId || Math.floor(Math.random()*1000000),
+            orderId: search.orderId || $search.orderId || Math.floor(Math.random()*1000000)
           };
 
           localStorageService.add('order', order);
         }
 
         $log.info('order', order);
-        return order
+        return order;
     })
     .factory('APISERVER', function(HOST, $location) {
     	var host = /nonobank.com/.test($location.host()) ? $location.protocol() + '://' + $location.host() : HOST;
