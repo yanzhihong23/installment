e5afa81bf241fb7794d8fd29eed244a5d87cdc69
Hom <yanzhihong23@qq.com>
2015-09-11 11:35:58 +0800

    move orderId and openId init to run block

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -7,27 +7,8 @@
     .constant('MERCHANT', '10000')
     .constant('PRODUCTID', '89')
     .factory('ORDER', function($location, $log, utils, localStorageService) {
-        var search = utils.getLocationSearch(),
-            $search = $location.search(),
-            order = localStorageService.get('order');
-
-        var _openId = search.openId || $search.openId || Math.floor(Math.random()*1000000),
-            _orderId = search.orderId || $search.orderId || Math.floor(Math.random()*1000000);
-
-        if(!order || (order.openId !== _openId)) {
-          order = {
-            openId: _openId,
-            orderId: _orderId
-          };
-
-          // localStorageService.remove('user');
-          localStorageService.clearAll();
-        } else if(order.openId === _openId && order.orderId !== _orderId) {
-          order.orderId = _orderId;
-        }
-
-        localStorageService.add('order', order);
-
+        var order = localStorageService.get('order');
+        
         $log.info('order', order);
         return order;
     })
--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,16 +6,38 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log, $rootScope, $ionicLoading, userService) {
+  function runBlock($log, $rootScope, $ionicLoading, userService, $location, utils, localStorageService) {
   	$rootScope.passwordPattern = /^(?!\d+$|[a-zA-Z]+$|[\W-_]+$)[\s\S]{6,16}$/;
 
     $rootScope.$on('loading:hide', function() {
       $ionicLoading.hide();
     });
 
+    // orderId & openId init
+    var search = utils.getLocationSearch(),
+        $search = $location.search(),
+        order = localStorageService.get('order');
+
+    var _openId = search.openId || $search.openId || Math.floor(Math.random()*1000000),
+        _orderId = search.orderId || $search.orderId || Math.floor(Math.random()*1000000);
+
+    if(!order || (order.openId !== _openId)) {
+      order = {
+        openId: _openId,
+        orderId: _orderId
+      };
+
+      localStorageService.remove('user', 'mId', 'sessionId');
+    } else if(order.openId === _openId && order.orderId !== _orderId) {
+      order.orderId = _orderId;
+    }
+
+    localStorageService.add('order', order);
+
     // auto login
-    $log.info('runBlock');
     userService.autoLogin();
+
+    $log.info('runBlock end');
   }
 
 })();
